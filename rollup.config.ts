ac298ae7b3ba4d18724f5323fc158196f0fddfc6
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-12-10 23:30:51 +0300

    build process fixes

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,7 +5,7 @@ export default {
   input: 'src/index.ts',
   output: [
     { file: 'dist/fetch-to-tar.js', name: 'fetchToTar', format: 'umd', sourcemap: true },
-    { file: 'dist/fetch-to-tar.m.js', format: 'esm', sourcemap: true },
+    { file: 'dist/fetch-to-tar.mjs', format: 'esm', sourcemap: true },
   ],
   plugins: [
     typescript({ useTsconfigDeclarationDir: true }),
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf dist
 ./node_modules/.bin/rollup -c rollup.config.ts
 ls dist/*.d.ts | grep -v index.d.ts | xargs rm
 mv dist/index.d.ts dist/fetch-to-tar.d.ts
