2cf022c59f0af4446e81e7b04b6f024894dafc66
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-12-11 14:56:23 +0300

    fix cursor bug in ram

--- src/ram.ts
+++ src/ram.ts
@@ -1,10 +1,10 @@
 export default class Ram {
-  cursor: number = 0;
+  cursor: number = -1;
   private blobs: Blob[] = [];
 
   async addBlob(blob: Blob) {
-    this.blobs.push(blob);
     this.cursor += 1;
+    this.blobs.push(blob);
   }
 
   async putBlob(key: any, blob: Blob) {
